de4e1917faf17700c3f311b2c26d28059366877f
PhamBang <phambang.dev@gmail.com>
2023-10-30 16:13:08 +0700

    feat: add new auth

---
+++ src/views/Auth/LoginForm.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="auth__right">
+    <div class="auth__header">
+      <div class="auth__header-logo">
+        <SvgIcon :icon="'logos:zenhub-icon'" :size="30"></SvgIcon>
+      </div>
+      <h3 class="auth__header-title">System Manager</h3>
+      <p class="auth__header-subtitle">
+        A management system is in place for analytics and informative and just beautiful.
+      </p>
+    </div>
+    <div class="auth__form">
+      <el-form :labelPosition="'top'" ref="formRef" class="form">
+        <el-form-item label="Email">
+          <InputBase
+            name="email"
+            rules="required|email"
+            placeholder="Please input your username!"
+            style="width: 100%"
+          />
+        </el-form-item>
+
+        <el-form-item label="Password">
+          <PasswordBase
+            name="password"
+            rules="required"
+            placeholder="Please input your password!"
+            style="width: 100%"
+          />
+        </el-form-item>
+
+        <el-form-item jus>
+          <el-col :span="16">
+            <el-checkbox v-model:checked="ruleForm.remember">Remember me</el-checkbox>
+          </el-col>
+          <el-col :span="8" style="text-align: end">
+            <el-link href="#" disabled>Recovery Password</el-link>
+          </el-col>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button size="large" type="primary" class="btn-submit mb-10" :disabled="isLoading" @click="onSubmit"
+            >Sign In
+          </el-button>
+          <span class="w-100 text-center"><strong>OR</strong></span>
+
+          <el-row :gutter="10" justify="center" class="w-100">
+            <el-button size="large" class="btn-login--social" @click="handleLoginWith" disabled circle>
+              <SvgIcon :icon="'flat-color-icons:google'" :size="24" />
+            </el-button>
+            <el-button size="large" class="btn-login--social" @click="handleLoginWith" disabled circle>
+              <SvgIcon :icon="'logos:facebook'" :size="24" />
+            </el-button>
+          </el-row>
+
+          <el-divider />
+
+          <el-row :gutter="10" justify="center" class="w-100">
+            <span class="mr-5">Don't have an account yet ? </span>
+            <el-link href="#"> Sign Up</el-link>
+          </el-row>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+import { useForm } from "vee-validate";
+import { useRouter } from "vue-router";
+
+import InputBase from "@/components/form/InputBase.vue";
+import PasswordBase from "@/components/form/PasswordBase.vue";
+
+import { useAuthStore } from "@/stores/auth.store";
+import type { LoginInput } from "@/core/interfaces/auth.interface";
+import SvgIcon from "@/components/common/SvgIcon.vue";
+import useAuth from "@/composables/useAuth";
+// import { transformErrors } from "@/shared/utils/transformErrors";
+
+const router = useRouter();
+const authStore = useAuthStore();
+const { doLogin, response, errors, isLoading } = useAuth();
+
+const ruleForm = ref<LoginInput>({
+  email: "",
+  password: "",
+  remember: false,
+});
+
+const { handleSubmit, setErrors } = useForm({ initialValues: { ...ruleForm.value } });
+
+const onSubmit = handleSubmit(async (values, actions) => {
+  await doLogin(values);
+  console.log(response.value);
+
+  const { shop, tokens } = response.value;
+  if (shop) authStore.setUserInfo(shop);
+  if (tokens) authStore.setToken(tokens);
+
+  if (errors.value) {
+    console.log("errors:::", errors.value);
+  }
+
+  await router.push("/dashboard");
+
+  actions.resetForm();
+});
+
+const handleLoginWith = () => {};
+</script>
---
+++ src/views/Auth/LoginSwiper.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="slide slide__login">
+    <swiper
+      class="slide__wrap"
+      :modules="modules"
+      :centeredSlides="true"
+      :spaceBetween="60"
+      :autoplay="autoPlay"
+      :pagination="pagination"
+      :scrollbar="{ draggable: true }"
+      @swiper="onSwiper"
+      @slideChange="onSlideChange"
+    >
+      <swiper-slide class="slide__item">
+        <LoginDesignComponent class="slide__img" />
+        <div class="slide__content">
+          <h3 class="slide__title">Real-time Data Synchronization</h3>
+          <p class="slide__para">
+            Ensures that any changes in the control system are instantly reflected in the manager, providing users with
+            up-to-the-moment information.
+          </p>
+        </div>
+      </swiper-slide>
+      <swiper-slide class="slide__item">
+        <LoginProcess class="slide__img" />
+        <div class="slide__content">
+          <h3 class="slide__title">Modular Component Architecture</h3>
+          <p class="slide__para">
+            Allows for the creation of modular and reusable components. Each component can represent a specific aspect
+            of the control system, promoting code organization and maintainability.
+          </p>
+        </div>
+      </swiper-slide>
+      <swiper-slide class="slide__item">
+        <LoginSolutionMindSet class="slide__img" />
+        <div class="slide__content">
+          <h3 class="slide__title">State Management With Pinia</h3>
+          <p class="slide__para">
+            Ensures a centralized and predictable state management system, crucial for maintaining the integrity of the
+            control system's data.
+          </p>
+        </div>
+      </swiper-slide>
+      <div class="swiper-pagination slide__pagination"></div>
+    </swiper>
+  </div>
+</template>
+<script setup lang="ts">
+import { reactive } from "vue";
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from "swiper/vue";
+// import required modules
+import { Autoplay, Pagination } from "swiper/modules";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { PaginationOptions } from "swiper/types";
+
+import LoginProcess from "@/components/svgs/LoginProcess.vue";
+import LoginDesignComponent from "@/components/svgs/LoginDesignComponent.vue";
+import LoginSolutionMindSet from "@/components/svgs/LoginSolutionMindSet.vue";
+
+const modules = reactive([Pagination, Autoplay]);
+const autoPlay = reactive({
+  delay: 3500,
+  disableOnInteraction: false,
+});
+
+const pagination = reactive<PaginationOptions>({
+  el: ".swiper-pagination",
+  type: "bullets",
+  clickable: true,
+});
+
+const onSwiper = (swiper: any) => {
+  console.log(swiper);
+};
+
+const onSlideChange = () => {};
+</script>
