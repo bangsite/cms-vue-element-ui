f40531996bf562263784ad2baf75a132beb28b3c
PhamBang <phambang.dev@gmail.com>
2023-11-08 17:09:36 +0700

    feat: update ui register, map api register

--- src/services/config/config.axios.ts
+++ src/services/config/config.axios.ts
@@ -7,5 +7,5 @@ const HEADER_API_KEY = import.meta.env.VITE_APP_API_KEY;
 export const config: AxiosRequestConfig = {
   baseURL: BASE_URL,
   headers: { Authorization: HEADER_AUTH, "x-api-key": HEADER_API_KEY },
-  withCredentials: true,
+  // withCredentials: true,
 };
